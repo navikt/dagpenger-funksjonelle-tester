abff1e2bdd1af190977b0a7275e3791c45294322
Giao The Cung <giaothe@gmail.com>
2020-08-19 13:47:33 +0200

    Upgrade constants

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -24,7 +24,7 @@ object Cucumber {
 object Dagpenger {
 
     object Biblioteker {
-        const val version = "2020.07.30-12.21.8857eb1469bf"
+        const val version = "2020.08.19-13.35.3a044ff6fea7"
         const val stsKlient = "com.github.navikt.dp-biblioteker:sts-klient:$version"
         const val grunnbeløp = "com.github.navikt.dp-biblioteker:grunnbelop:$version"
         const val ktorUtils = "com.github.navikt.dp-biblioteker:ktor-utils:$version"
@@ -45,7 +45,7 @@ object Dagpenger {
         }
     }
 
-    const val Streams = "com.github.navikt:dagpenger-streams:2020.05.15-23.03.e0ab571caee4"
+    const val Streams = "com.github.navikt:dagpenger-streams:2020.08.19-13.32.0fd360f3ef11"
     const val Events = "com.github.navikt:dagpenger-events:2020.08.19-10.57.d2fe892352eb"
 }
 
@@ -156,7 +156,7 @@ object Kotlinx {
 }
 
 object Ktor {
-    const val version = "1.3.2"
+    const val version = "1.4.0"
     const val server = "io.ktor:ktor-server:$version"
     const val serverNetty = "io.ktor:ktor-server-netty:$version"
     const val auth = "io.ktor:ktor-auth:$version"
